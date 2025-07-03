a856ed365682b5125f18f79631fa159c603300f3
Andres Reyes <alonso270193@gmail.com>
2025-07-03 13:02:17 -0600

    appservprov

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,16 +22,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (app()->environment('production')) {
+            // Configurar trusted proxies para Railway
+            $this->app['request']->setTrustedProxies(
+                ['*'],
+                \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+                    \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+                    \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+                    \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO
+            );
+
             URL::forceScheme('https');
-
-            // Configurar para Railway
-            if (request()->header('X-Forwarded-Proto') === 'https') {
-                request()->server->set('HTTPS', 'on');
-                request()->server->set('SERVER_PORT', 443);
-            }
-
-            // Forzar root URL
-            URL::forceRootUrl(env('APP_URL'));
+            URL::forceRootUrl(config('app.url'));
         }
         // if (env('APP_ENV') === 'production') {
         //     URL::forceScheme('https');
